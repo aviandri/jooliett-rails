fdbb8608493777fce8eeee5501fccc9323d6a3ef
aviandri <aviandri@gmail.com>
2013-07-29 20:19:00 +0700

    remove confilcting migration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,5 @@
 require "bundler/capistrano"
+require "rvm/capistrano"
 
 server "192.241.150.114", :web, :app, :db, primary: true
 
@@ -52,11 +53,6 @@ end
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
