a60a803ebe6b52c04bbcfa8b901ab4dfafbd4e75
aviandri <aviandri@gmail.com>
2013-08-02 11:30:01 +0700

    remove uneeded files

--- spec/models/invalidate_cart_worker_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe InvalidateCartWorker do
-	describe "should add job" do		
-		InvalidateCartWorker.perform_async(1)
-		InvalidateCartWorker.perform_in(1.hour, 2)
-	end
-end
