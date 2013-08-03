245242db16f92f429887e5a5387189f15fd3767f
aviandri <aviandri@gmail.com>
2013-08-04 05:37:59 +0700

    fix responsive

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require jquery_ujs
 //= require jquery.elevatezoom
 //= require bootstrap.min
-//= require navbar_fix.js
 //= require html5shiv
 //= require active_admin
 //= require cart_items
